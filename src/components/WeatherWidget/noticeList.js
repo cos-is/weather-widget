c0f07e841bd5da86fd7420b2b7b6dbff22056096
Constantine <konstantin@phoenix-cg.ru>
2021-02-08 15:43:55 +0500

    Edit notice list

--- src/components/WeatherWidget/noticeList.js
+++ src/components/WeatherWidget/noticeList.js
@@ -1,5 +1,7 @@
 export default {
   networkError: 'You\'re offline. Check your connection.',
-  notFound: 'Can\'t find a city with such name. Try another one.',
-  noData: 'No weather to show yet. '
+  404: 'Can\'t find a city with such name. Try another one.',
+  noData: 'No weather to show yet. ',
+  cityExists: 'You have already configured this city.',
+  default: 'Something gone wrong'
 }
