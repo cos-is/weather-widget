e320cb80a0b74fea8c7005355315c7be5b322e62
Constantine <konstantin@phoenix-cg.ru>
2021-02-08 15:48:26 +0500

    Add Settings view

--- src/components/WeatherWidget/WeatherWidget.vue
+++ src/components/WeatherWidget/WeatherWidget.vue
@@ -1,12 +1,16 @@
 <template>
   <div class="weather-widget">
     <div v-if="notice || cities.length === 0" class="weather-widget_notice">{{ notice || noticeList.noData }}</div>
+    <icon-button icon="settings" class="weather-widget_toggle-settings" @click="openSettings" />
     <city-item
       v-for="city in cities"
-      :key="city.sys.id"
+      :key="city.id"
       :city="city"
       class="weather-widget_city"
     />
+    <fade-transition>
+      <settings-view v-if="settingsVisible" :notice="settingsNotice" :cities="cities" @add-city="addCity" @update-cities="updateCities" @close="closeSettings" />
+    </fade-transition>
   </div>
 </template>
 
@@ -14,19 +18,26 @@
 import { LocalStore } from '@/utils'
 import { getWeatherData } from '@/api/weather.api'
 import CityItem from './CityItem'
+import IconButton from '@ui/IconButton'
+import { FadeTransition } from 'vue2-transitions'
+import SettingsView from './SettingsView'
 import noticeList from './noticeList'
 
 const CitiesStore = new LocalStore('cities')
 
 export default {
   components: {
-    CityItem
+    CityItem,
+    IconButton,
+    FadeTransition,
+    SettingsView
   },
   data () {
     return {
       cities: [],
       notice: null,
       settingsNotice: null,
+      settingsVisible: false
     }
   },
   computed: {
@@ -75,18 +86,57 @@ export default {
           this.notice = noticeList.networkError
         } else {
           this.settingsNotice = noticeList[response.status] || noticeList.default
-      }
+        }
       }
     },
     async requestSavedCities () {
       for (const city of CitiesStore.item) {
         await this.addCity({ q: city }, false)
       }
+    },
+    openSettings () {
+      this.settingsVisible = true
+    },
+    closeSettings () {
+      this.settingsVisible = false
+    },
+    updateCities (cities) {
+      this.cities = cities
+      this.saveCities()
     }
   }
 }
 </script>
 
-<style>
-
+<style lang="scss">
+  .weather-widget {
+    --c-grey: #484848;
+    --c-grey-light: #e3e3e3;
+    --c-red: #f48f8f;
+    display: inline-block;
+    padding: 20px;
+    font-family: 'Roboto', sans-serif;
+    font-size: 14px;
+    position: relative;
+    min-width: 200px;
+    &,
+    & * {
+      box-sizing: border-box;
+    }
+  }
+  .svg-icon {
+    width: 20px;
+    height: 20px;
+    color: var(--c-grey);
+  }
+  .weather-widget_toggle-settings {
+    position: absolute;
+    right: 15px;
+    top: 15px;
+  }
+  .weather-widget_notice {
+    padding: 20px;
+    margin-top: 30px;
+    background: var(--c-grey-light);
+  }
 </style>
