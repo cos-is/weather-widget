2a442cd5ddd500b15c0761eef138caca2fb66929
Constantine <konstantin@phoenix-cg.ru>
2021-02-08 15:40:17 +0500

    Move key to .env

--- src/api/http.js
+++ src/api/http.js
@@ -1,16 +1,14 @@
 import axios from 'axios'
 
 const defaultRequestSettings = {
-  appid: '73e502f8d35943e7ff6ca686630cb29e',
+  appid: process.env.VUE_APP_API_KEY,
   units: 'metric'
 }
 
 const config = {}
 
 const http = axios.create(config)
-// http.defaults.params =
 http.interceptors.request.use((req) => {
-  console.log({ ...req })
   req.params = { ...defaultRequestSettings, ...req.params }
   return req
 })
