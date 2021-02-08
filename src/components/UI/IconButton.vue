f365ecc81de19c8f07ee7d08b719586c815ea83d
Constantine <konstantin@phoenix-cg.ru>
2021-02-08 15:43:02 +0500

    IconButton

---
+++ src/components/UI/IconButton.vue
@@ -0,0 +1,26 @@
+<template>
+  <button class="icon-button" @click="$emit('click')">
+    <svgicon :name="icon" original />
+  </button>
+</template>
+
+<script>
+export default {
+  props: {
+    icon: {
+      type: String,
+      required: true
+    }
+  }
+}
+</script>
+
+<style lang="scss">
+  .icon-button {
+    display: inline-flex;
+    padding: 5px;
+    border: none;
+    cursor: pointer;
+    background: transparent;
+  }
+</style>
